af47fa92691e48ed24d47c3fc0285934eaf4ac67
Toshich <shivalinanton@yandex.ru>
2024-12-04 20:45:03 +0300

    First commit

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var counter: Int = 0
 
+    @IBOutlet weak var buttonReset: UIButton!
+    @IBOutlet weak private var HistoryOfChanges: UITextView!
+    @IBOutlet weak var buttonReduceCounter: UIButton!
+    @IBOutlet weak var valueOfcounter: UILabel!
+    @IBOutlet weak var buttonAddCouner: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func increaseCounter(_ sender: Any) {
+        counter += 1
+        valueOfcounter.text = "Значение счётчика: \(counter)"
+        HistoryOfChanges.text += "\(Date()): значение изменено на +1 \n"
+    }
+    
+    @IBAction func reduceCounter(_ sender: Any) {
+        if counter <= 0 {
+            HistoryOfChanges.text += "\(Date()): попытка уменьшить значение счётчика ниже 0 \n"
+            return
+        }
+        counter -= 1
+        valueOfcounter.text = "Значение счётчика: \(counter)"
+        HistoryOfChanges.text += "\(Date()) и \(Calendar.current): значение изменено на -1 \n"
+    }
+    
+    
+    @IBAction func resetCounter(_ sender: Any) {
+        counter = 0
+        valueOfcounter.text = "Значение счётчика: \(counter)"
+        HistoryOfChanges.text += "\(Date()): значение сброшено \n"
+    }
 }
+
 
